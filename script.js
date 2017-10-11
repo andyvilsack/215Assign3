55260e2eb9bb808f98e8a48b1db8fa49365812ef
Smith <dds1010@sru.edu>
2017-10-11 16:05:13 -0400

    updates

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function addImage(name) {
 	var picName = name;
 	imageArray.push('anim/' + picName + '.jpg');
 	console.log(imageArray);
-
+    
+    var img = document.createElement("img");
+    img.src = 'anim/' + picName + '.jpg'; 
+    document.body.appendChild(img);
 }
 
