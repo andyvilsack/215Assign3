c9a9199965f24e1510280d8fc234baaff7a6c56f
John-George Sample <Johngeorgesample@gmail.com>
2017-10-12 13:02:17 -0400

    Finished the project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var imageArray = new Array;
+i = 0;
 
 function addImage(name) {
 	var picName = name;
@@ -6,7 +7,47 @@ function addImage(name) {
 	console.log(imageArray);
     
     var img = document.createElement("img");
-    img.src = 'anim/' + picName + '.jpg'; 
-    document.body.appendChild(img);
+    img.src = 'anim/' + picName + '.jpg';
+    img.style.width='100px';
+    document.getElementById("putStuffHere").appendChild(img);
 }
+
+function startAnim() {
+	if(imageArray.length > 0) {
+
+
+		document.getElementById("tableDiv").innerHTML = "<img style='margin: 1px' src='" + imageArray[0] + "'>"; 
+
+		timer = setInterval(function() {
+			document.getElementById("tableDiv").innerHTML ="<img style='margin: 1px' src='" + imageArray[i] + "'>";
+			if(i<imageArray.length-1) {
+				i++;
+			}
+
+			else if(i>=imageArray.length-1) {
+				i=0;
+			}
+			
+		},500);
+	}
+
+	else {
+		alert("Error: need to add at least one image");
+	}
+}
+
+function stopAnim() {
+	clearInterval(timer);
+	i=0;
+	document.getElementById("tableDiv").innerHTML = "<img style='margin: 1px' src='" + imageArray[0] + "'>";
+}
+
+function popOff() {
+	imageArray.pop();
+
+	var putStuffHere = document.getElementById('putStuffHere');
+	//maybe put a conditional to prevent errors in console
+	putStuffHere.removeChild(putStuffHere.lastChild);
+	console.log(imageArray);
+} 
 
